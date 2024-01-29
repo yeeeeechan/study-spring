e11d4bcf38d43bc174fba2fbc9da759bc46bf6df
yeeeeechan <leeyechan6@gmail.com>
2024-01-29 21:41:56 +0900

    study: spring

---
+++ src/test/java/study/studyspring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package study.studyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
